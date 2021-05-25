6c1398b179b8a173d30c9e16486849bbf2f24d3d
benji-35 <benjamin.delvert@epitech.eu>
2021-05-25 19:20:12 +0200

    FIX : if str equal null

--- kap-lib/kap/kapstr/str_to_lowercase.c
+++ kap-lib/kap/kapstr/str_to_lowercase.c
@@ -9,6 +9,8 @@
 
 void str_to_lower_case(string str)
 {
+    if (str == KNULL)
+        return;
     for (int i = 0; str[i] != 0; i++) {
         if (str[i] >= 'A' && str[i] <= 'Z')
             str[i] = str[i] + 32;
@@ -17,6 +19,8 @@ void str_to_lower_case(string str)
 
 void str_to_upper_case(string str)
 {
+    if (str == KNULL)
+        return;
     for (int i = 0; str[i] != 0; i++) {
         if (str[i] >= 'a' && str[i] <= 'z')
             str[i] = str[i] - 32;
