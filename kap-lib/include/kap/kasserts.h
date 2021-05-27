9c24f4ac9889ce29789e1b1ce1e728f440fdca3a
benji-35 <benjamin.delvert@epitech.eu>
2021-05-27 09:42:50 +0200

    ADD : header file only for asserts

---
+++ kap-lib/include/kap/kasserts.h
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2021
+** C-Lib
+** File description:
+** kasserts
+*/
+
+#ifndef _KAP_ASSERTS_H_
+#define _KAP_ASSERTS_H_
+
+#include <stdbool.h>
+
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
+
+#ifndef _KAP_STR_TYPD_
+#define _KAP_STR_TYPD_
+
+typedef char ** text;
+typedef char * string;
+typedef char const * cstring;
+typedef char const ** ctext;
+
+#endif /*_KAP_STR_TYPD_*/
+
+#ifndef kASSERT_FUNCTION
+#define kASSERT_FUNCTION NULL
+#endif /*kASSERT_FUNCTION*/
+
+extern bool kassert_e(bool isfail, cstring __expr, cstring __fname, int __line);
+extern bool kassert_f(bool isfail, cstring __expr, cstring __fname, int __line);
+
+#define kassert(__expr) kassert_f(__expr, #__expr, __FILE__, __LINE__)
+#define keassert(__expr) kassert_e(__expr, #__expr, __FILE__, __LINE__)
+
+#endif /* !_KAP_ASSERTS_H_ */
