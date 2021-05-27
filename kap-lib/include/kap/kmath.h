32a306b836d22c1aa4ca4d15be0b5e3f26a4e112
benji-35 <benjamin.delvert@epitech.eu>
2021-05-27 10:35:37 +0200

    CHG : add descrption in header file for maths function

--- kap-lib/include/kap/kmath.h
+++ kap-lib/include/kap/kmath.h
@@ -14,8 +14,21 @@
 #define ABS(value)    (((value) < (0)) ? (value * -1) : (value))
 #endif /*ABS*/
 
+/// fucntion that return if num can be divide by div
+/// @param num > number to divide
+/// @param div > number that divide num
+/// @return true if num can be divide by div
 bool kdiv_by(kssize_t num, kssize_t div);
+
+/// function that return the power
+/// @param num > number to power
+/// @param power > power value
+/// @return result of power
 kdsize_t kpower(ksize_t num, ksize_t power);
+
+/// function that return the square root
+/// @param nb > number to execute the square root
+/// @return result of square root
 kdsize_t ksquare_root(kussize_t nb);
 
 #endif /* !_KAP_MATH_H */
--- kap-lib/kap/kmaths/kdivisible_by.c
+++ kap-lib/kap/kmaths/kdivisible_by.c
@@ -9,9 +9,9 @@
 
 bool kdiv_by(kssize_t num, kssize_t div)
 {
-    double f = (double)num / (double)div;
-    int i = num / div;
-    if ((double)i == f)
+    kdsize_t f = (kdsize_t)num / (kdsize_t)div;
+    ksize_t i = num / div;
+    if ((kdsize_t)i == f)
         return (true);
     return (false);
 }
