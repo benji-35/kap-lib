03b9f0e22e1651676139cba94c9247d56b45bfd4
benji-35 <benjamin.delvert@epitech.eu>
2021-05-27 10:15:57 +0200

    FIX : length_text that get bad arg

--- kap-lib/kap/kutils/check_file_extension.c
+++ kap-lib/kap/kutils/check_file_extension.c
@@ -17,7 +17,7 @@ bool check_extension_file(string path, string __exten)
     kassert((sep = split_str(path, '.')) == NULL);
     if (sep == NULL)
         return false;
-    kassert((len = length_text(len)) <= 0);
+    kassert((len = length_text(sep)) <= 0);
     if (len <= 0)
         return false;
     if (str_equality(sep[len - 1], __exten))
