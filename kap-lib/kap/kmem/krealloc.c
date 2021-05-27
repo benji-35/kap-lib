d9e43a458d585a470322f4ec017b7d8c2f16859f
benji-35 <benjamin.delvert@epitech.eu>
2021-05-27 10:21:51 +0200

    CHG : add krealloc_sec

--- kap-lib/kap/kmem/krealloc.c
+++ kap-lib/kap/kmem/krealloc.c
@@ -23,3 +23,20 @@ void *krealloc(void *__ptr, ksize_t __size, ksize_t __new_size)
     kfree(__ptr);
     return __nptr;
 }
+
+void *krealloc_sec(void *__ptr, ksize_t __size, ksize_t __new_size)
+{
+    void *__nptr;
+
+    if (!__size || !__new_size) {
+        kfree(__ptr);
+        return NULL;
+    }
+    kassert((__nptr = kcalloc_sec(__new_size)) == NULL && "krealloc ->\
+    failed\n");
+    if (!__nptr)
+        return __ptr;
+    kmemcpy(__nptr, __ptr, __size);
+    kfree(__ptr);
+    return __nptr;
+}
