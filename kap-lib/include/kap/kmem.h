5a72b5a95f86a766d3134afb39bbb6f9c43ab36a
benji-35 <benjamin.delvert@epitech.eu>
2021-05-27 10:01:11 +0200

    CHG : add kmalloc as proto + set description of functions

--- kap-lib/include/kap/kmem.h
+++ kap-lib/include/kap/kmem.h
@@ -27,10 +27,43 @@ typedef uchar_t uint8_t;
 #define NULL ((void *)0)
 #endif
 
+///kmemset will set pointer value
+///@param __ptr > pointer to set value
+///@param __val > value to set
+///@param __size > size of the pointer
 void kmemset(void *__ptr, int __val, ksize_t __size);
+
+///kmemcpy copy valu of pointer __src to pointer __ptr
+///@param __ptr > pointer that get value copied
+///@param __src > pointer with value to copy
+///@param __size > size of __src pointer
+///@return void * > return __ptr after copied (argument __ptr will update)
 void *kmemcpy(void *__ptr, void *__src, ksize_t __size);
+
+///same as free of stdlib BUT if pointer is NULL, the function do nothing
+///@param __ptr > pointer to free
 void kfree(void *__ptr);
+
+///same as malloc or kmalloc BUT set your pointer to 0
+///@param __size > size of malloc
+///@return void * > return result of malloc
 void *kcalloc(ksize_t __size);
+
+///kmalloc_sec return the malloc BUT if malloc return NULL, programm is exit 84
+///@param __ptr > pointer to realloc
+///@param __size > current size of pointer
+///@param __new_size > new size of malloc
+///@return void * > return result of remalloc
 void *krealloc(void *__ptr, ksize_t __size, ksize_t __new_size);
+
+///kmalloc return the malloc
+///@param __size > size of malloc
+///@return void * > return result of malloc
+void *kmalloc(ksize_t __size);
+
+///kmalloc_sec return the malloc BUT if malloc return NULL, programm is exit 84
+///@param __size > size of malloc
+///@return void * > return result of malloc
+void *kmalloc_sec(ksize_t __size);
 
 #endif /* !_KAP_MEM_H_ */
