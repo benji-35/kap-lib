a674fd9655f7acc676d182063f477eee7cd75a6f
benji-35 <benjamin.delvert@epitech.eu>
2022-01-12 18:27:37 +0100

    CHG : add some texts in start_test

--- start_tests.sh
+++ start_tests.sh
@@ -6,20 +6,78 @@
 ##
 
 clear
+
+echo "Do you want clear warnings after compiling ?"
+read needClear
+echo "Do you want play main.c test file"
+read playmain
+echo "Do you want play tests"
+read playtests
+
+clear
+
 make fclean -C tests/
 make -C tests/
-libFile=./kap-lib/libkap.a
 
+libFile=./kap-lib/libkap.a
 libMainExe=./tests/main_parser
 
-if [ -e "$libFile" ]; then
-    clear
-    tests/tests
-    if [ -e "$libMainExe" ]; then
-        valgrind $libMainExe
-        rm -f libMainExe
+if [ -e "$libFile" ]
+then
+    if [ "$needClear" == "y" ]
+    then
+        clear
+    elif [ "$needClear" == "Y" ]
+    then
+        clear
+    elif [ "$needClear" == "yes" ]
+    then
+        clear
+    elif [ "$needClear" == "YES" ]
+    then
+        clear
+    fi
+
+    if [ -e "tests/tests" ]
+    then
+        if [ "$playtests" == "y" ]
+        then
+            tests/tests
+        elif [ "$playtests" == "Y" ]
+        then
+            tests/tests
+        elif [ "$playtests" == "yes" ]
+        then
+            tests/tests
+        elif [ "$playtests" == "YES" ]
+        then
+            tests/tests
+        fi
+
+        if [ -e "$libMainExe" ]
+        then
+            if [ "$playmain" == "y" ]
+            then
+                valgrind $libMainExe
+                rm -f libMainExe
+            elif [ "$playmain" == "Y" ]
+            then
+                valgrind $libMainExe
+                rm -f libMainExe
+            elif [ "$playmain" == "yes" ]
+            then
+                valgrind $libMainExe
+                rm -f libMainExe
+            elif [ "$playmain" == "YES" ]
+            then
+                valgrind $libMainExe
+                rm -f libMainExe
+            fi
+        else
+            echo "$libMainExe do not exists"
+        fi
     else
-        echo "$libMainExe do not exists"
+        echo "test file not found"
     fi
     make fclean -C tests/
 else
