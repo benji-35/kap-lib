6af5309d80cc8c56cfa52961655e3a7414358336
benji-35 <benjamin.delvert@epitech.eu>
2022-04-28 11:55:19 +0200

    CHG : add var typ

--- kap-lib/include/kap/kutils.h
+++ kap-lib/include/kap/kutils.h
@@ -6,138 +6,142 @@
 */
 
 #ifndef _KAP_UTILS_H
-#define _KAP_UTILS_H
+    #define _KAP_UTILS_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdbool.h>
-#include <kap/kmem.h>
-#include <kap/kasserts.h>
+    #include <stdio.h>
+    #include <unistd.h>
+    #include <stdbool.h>
+    #include <kap/kmem.h>
+    #include <kap/kasserts.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+    #ifdef __cplusplus
+        extern "C" {
+    #endif
 
-#ifndef _KAP_SIZE_
-#define _KAP_SIZE_
+    #ifndef _KAP_SIZE_
+        #define _KAP_SIZE_
 
-typedef unsigned long kusize_t;
-typedef unsigned long long kussize_t;
-typedef long ksize_t;
-typedef long long kssize_t;
-typedef long double kdsize_t;
-typedef unsigned char uchar_t;
-typedef uchar_t uint8_t;
+        typedef unsigned long kusize_t;
+        typedef unsigned long long kussize_t;
+        typedef long ksize_t;
+        typedef long long kssize_t;
+        typedef long double kdsize_t;
+        typedef unsigned char uchar_t;
+        typedef uchar_t uint8_t;
 
-#endif /*_KAP_SIZE_*/
+    #endif /*_KAP_SIZE_*/
 
-#ifndef _KAP_STR_TYPD_
-#define _KAP_STR_TYPD_
+    #ifndef _KAP_STR_TYPD_
+        #define _KAP_STR_TYPD_
 
-typedef char ** text;
-typedef char * string;
-typedef char const * cstring;
-typedef char const ** ctext;
+        typedef char ** text;
+        typedef char * string;
+        typedef char const * cstring;
+        typedef char const ** ctext;
 
-#endif /*_KAP_STR_TYPD_*/
+    #endif /*_KAP_STR_TYPD_*/
 
-#ifndef false
-#define false 0
-#endif
+    #ifndef false
+        #define false 0
+    #endif
 
-#ifndef true
-#define true 1
-#endif
+    #ifndef true
+        #define true 1
+    #endif
 
-#ifndef KNULL
-#define KNULL ((void *)0)
-#endif /*KNULL*/
+    #ifndef KNULL
+        #define KNULL ((void *)0)
+    #endif /*KNULL*/
 
-#ifndef OBJECT
-#define OBJECT void *
-#endif
+    #ifndef OBJECT
+        #define OBJECT void *
+    #endif
 
-#ifndef PVOID
-#define PVOID static void
-#endif /*PVOID*/
+    #ifndef PVOID
+        #define PVOID static void
+    #endif /*PVOID*/
 
-#ifndef PBOOL
-#define PBOOL static bool
-#endif /*PBOOL*/
+    #ifndef PBOOL
+        #define PBOOL static bool
+    #endif /*PBOOL*/
 
-#ifndef PINT
-#define PINT static int
-#endif /*PINT*/
+    #ifndef PINT
+        #define PINT static int
+    #endif /*PINT*/
 
-#ifndef PCHAR
-#define PCHAR static char
-#endif /*PCHAR*/
+    #ifndef PCHAR
+        #define PCHAR static char
+    #endif /*PCHAR*/
 
-#ifndef PSTRING
-#define PSTRING static string
-#endif /*PSTRING*/
+    #ifndef PSTRING
+        #define PSTRING static string
+    #endif /*PSTRING*/
 
-#ifndef PFLOAT
-#define PFLOAT static float
-#endif /*PFLOAT*/
+    #ifndef PFLOAT
+        #define PFLOAT static float
+    #endif /*PFLOAT*/
 
-#ifndef PDOUBLE
-#define PDOUBLE static double
-#endif /*PFLOAT*/
+    #ifndef PDOUBLE
+        #define PDOUBLE static double
+    #endif /*PFLOAT*/
 
-#ifndef _KAP_FD_
-#define _KAP_FD_
+    #ifndef _KAP_FD_
+    #define _KAP_FD_
 
-#define kstdin 0
-#define kstdout 1
-#define kstderr 2
+    #define kstdin 0
+    #define kstdout 1
+    #define kstderr 2
 
-#endif /*_KAP_FD_*/
+    #ifndef var
+        #define var __auto_type
+    #endif
 
-#ifndef UNUSED
-#define UNUSED __attribute__((unused))
-#endif /*UNUSED*/
+    #endif /*_KAP_FD_*/
 
-/// check if file can be open or not
-/// @param path > path of file
-/// @return true if file can be open or false if file can't be open
-extern bool can_open_file(cstring path);
+    #ifndef UNUSED
+        #define UNUSED __attribute__((unused))
+    #endif /*UNUSED*/
 
-/// open file and return content
-/// @param path > path of file
-/// @return content. If file can't be open return NULL
-extern string kopen_file(cstring path);
+    /// check if file can be open or not
+    /// @param path > path of file
+    /// @return true if file can be open or false if file can't be open
+    extern bool can_open_file(cstring path);
 
-/// open file and return content
-/// @param path > path of file
-/// @return content as text format. If file can't be open return NULL
-extern text kopen_file_t(cstring path);
+    /// open file and return content
+    /// @param path > path of file
+    /// @return content. If file can't be open return NULL
+    extern string kopen_file(cstring path);
 
-/// write string in file
-/// @param path > path of file
-/// @param str > string that will write in file
-/// @param append > if true : the str will write after file content. If false : the file will clear and then write str
-extern void kwrite_file(cstring path, cstring str, bool append);
+    /// open file and return content
+    /// @param path > path of file
+    /// @return content as text format. If file can't be open return NULL
+    extern text kopen_file_t(cstring path);
 
-/// write text in file
-/// @param path > path of file
-/// @param txt > text that will write in file
-/// @param sep > separator between lines in text (to make a real text, put \\n)
-/// @param append > if true : the str will write after file content. If false : the file will clear and then write str
-extern void kwrite_file_t(string path, text txt, cstring sep, bool append);
+    /// write string in file
+    /// @param path > path of file
+    /// @param str > string that will write in file
+    /// @param append > if true : the str will write after file content. If false : the file will clear and then write str
+    extern void kwrite_file(cstring path, cstring str, bool append);
 
-///clear a file
-///@param path > path of file
-extern void kclear_file(cstring path);
+    /// write text in file
+    /// @param path > path of file
+    /// @param txt > text that will write in file
+    /// @param sep > separator between lines in text (to make a real text, put \\n)
+    /// @param append > if true : the str will write after file content. If false : the file will clear and then write str
+    extern void kwrite_file_t(string path, text txt, cstring sep, bool append);
 
-/// check the extension of a file
-/// @param path > path of file
-/// @param __exten > extension want
-/// @return true if file have the good extension. False if it doesn't
-extern bool check_extension_file(cstring path, cstring __exten);
+    ///clear a file
+    ///@param path > path of file
+    extern void kclear_file(cstring path);
 
-#ifdef __cplusplus
-}
-#endif
+    /// check the extension of a file
+    /// @param path > path of file
+    /// @param __exten > extension want
+    /// @return true if file have the good extension. False if it doesn't
+    extern bool check_extension_file(cstring path, cstring __exten);
+
+    #ifdef __cplusplus
+        }
+    #endif
 
 #endif /* !_KAP_UTILS_H */
--- tests/main.c
+++ tests/main.c
@@ -10,101 +10,15 @@
 
 int main(int ac UNUSED, text av UNUSED)
 {
-    /*string bin = int_to_bin(1, true);
-    string hex = int_to_hex(1, true);
-    string hex_bin = bin_to_hex(bin, true, true);
-    string bin_hex = hex_to_bin(hex, true, true);
-    printf("(1) bin = %s, hex = %s, hex_bin = %s, bin_hex = %s\n", bin, hex, hex_bin, bin_hex);
-    printf("int value = %d, int_hex value = %d\n", bin_to_int(bin, true), hex_to_int(hex, true));
-    kfree(bin);
-    kfree(hex);
-    kfree(hex_bin);
-    kfree(bin_hex);
+    var coucou = copy_str("Hello World !");
+    var txt = split_str(coucou, ' ');
 
-
-    bin = int_to_bin(5, true);
-    hex = int_to_hex(5, true);
-    hex_bin = bin_to_hex(bin, true, true);
-    bin_hex = hex_to_bin(hex, true, true);
-    printf("(1) bin = %s, hex = %s, hex_bin = %s, bin_hex = %s\n", bin, hex, hex_bin, bin_hex);
-    printf("int value = %d, int_hex value = %d\n", bin_to_int(bin, true), hex_to_int(hex, true));
-    kfree(bin);
-    kfree(hex);
-    kfree(hex_bin);
-    kfree(bin_hex);
-
-
-    bin = int_to_bin(12, true);
-    hex = int_to_hex(12, true);
-    hex_bin = bin_to_hex(bin, true, true);
-    bin_hex = hex_to_bin(hex, true, true);
-    printf("(1) bin = %s, hex = %s, hex_bin = %s, bin_hex = %s\n", bin, hex, hex_bin, bin_hex);
-    printf("int value = %d, int_hex value = %d\n", bin_to_int(bin, true), hex_to_int(hex, true));
-    kfree(bin);
-    kfree(hex);
-    kfree(hex_bin);
-    kfree(bin_hex);
-
-
-    bin = int_to_bin(32, true);
-    hex = int_to_hex(32, true);
-    hex_bin = bin_to_hex(bin, true, true);
-    bin_hex = hex_to_bin(hex, true, true);
-    printf("(1) bin = %s, hex = %s, hex_bin = %s, bin_hex = %s\n", bin, hex, hex_bin, bin_hex);
-    printf("int value = %d, int_hex value = %d\n", bin_to_int(bin, true), hex_to_int(hex, true));
-    kfree(bin);
-    kfree(hex);
-    kfree(hex_bin);
-    kfree(bin_hex);
-
-
-    bin = int_to_bin(64, true);
-    hex = int_to_hex(64, true);
-    hex_bin = bin_to_hex(bin, true, true);
-    bin_hex = hex_to_bin(hex, true, true);
-    printf("(1) bin = %s, hex = %s, hex_bin = %s, bin_hex = %s\n", bin, hex, hex_bin, bin_hex);
-    printf("int value = %d, int_hex value = %d\n", bin_to_int(bin, true), hex_to_int(hex, true));
-    kfree(bin);
-    kfree(hex);
-    kfree(hex_bin);
-    kfree(bin_hex);
-
-
-    bin = int_to_bin(66, true);
-    hex = int_to_hex(66, true);
-    hex_bin = bin_to_hex(bin, true, true);
-    bin_hex = hex_to_bin(hex, true, true);
-    printf("(1) bin = %s, hex = %s, hex_bin = %s, bin_hex = %s\n", bin, hex, hex_bin, bin_hex);
-    printf("int value = %d, int_hex value = %d\n", bin_to_int(bin, true), hex_to_int(hex, true));
-    kfree(bin);
-    kfree(hex);
-    kfree(hex_bin);
-    kfree(bin_hex);*/
-    kyml_parser_t *parser = init_yml_parser("tests/parser_files/test.yml");
-    if (parser == NULL) {
-        kprintf("Error while init parser -> returned NULL\n");
-        return 84;
+    for (var i = 0; i < length_text(txt); i++) {
+        printf("%s\n", txt[i]);
     }
-    set_value_YML("content.content1", "finalContent", parser);
-    string val = get_value_YML("clans.allezLeFoot.color", parser);
-    if (val == NULL) {
-        kprintf("val from path : [clans.allezLeFoot.color] = []\n");
-        kprintf("[MAIN] creation du path avec la valeur initiale\n");
-        set_value_YML("clans.allezLeFoot.color", "Init value", parser);
-        kprintf("set [Init value] to [clans.allezLeFoot.color]\n");
-    } else if (str_equality(val, "&2") == false) {
-        kprintf("val from path : [clans.allezLeFoot.color] = [%s]\n", val);
-        set_value_YML("clans.allezLeFoot.color", "&2", parser);
-        kprintf("set [\"&2\"] to [clans.allezLeFoot.color]\n");
-        string nVal = get_value_YML("clans.allezLeFoot.color", parser);
-        kprintf("val from path : [clans.allezLeFoot.color] = [%s]\n", nVal);
-        kfree(nVal);
-    } else {
-        kprintf("everything good\n");
-    }
-    kprintf("\n");
-    //display_yml_content(parser);
-    save_yml_parser(parser);
-    destroy_yml_parser(parser);
+
+    free_text(txt);
+    kfree(coucou);
+
     return 0;
 }
