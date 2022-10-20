1935ffe1990d507eef971ce4c9ef21aa8dd14a47
benji-35 <benjamin.delvert@epitech.eu>
2022-10-20 10:49:40 +0200

    FIX : warning

--- kap-lib/kap/klist/list/kinit_list.c
+++ kap-lib/kap/klist/list/kinit_list.c
@@ -18,3 +18,16 @@ klist_t *list_init(void)
     res->size = 0;
     return (res);
 }
+
+klist_t *list_copy(klist_t *list) {
+    klist_t *result = NULL;
+    if (list == NULL)
+        return result;
+    result = kmalloc_sec(sizeof(klist_t));
+
+    for (kusize_t i = 0; i < list->size; i++) {
+        knode_t *node = list_get_inode(list, i);
+        list_add_node(result, node->data, node->print);
+    }
+    return result;
+}
